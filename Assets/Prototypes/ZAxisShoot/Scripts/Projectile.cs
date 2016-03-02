d594c7dba477c7bd1a263d9e250d1afd3e7e8ff5
Nujerk <junkerlucas@gmail.com>
2016-03-02 11:53:42 +0100

    Z Axis Shoot : a 2D prototype that allow to shoot in any direction with the second stick of a joystick

---
+++ Assets/Prototypes/ZAxisShoot/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    private float birthDate;
+    public float lifeDuration = 2;
+
+	// Use this for initialization
+	void Start () {
+        birthDate = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.realtimeSinceStartup - birthDate > lifeDuration)
+            Destroy(gameObject);
+	}
+}
