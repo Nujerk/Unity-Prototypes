d0afe1475d99cc20edf3a1a92a008a6033c0e9dd
Nujerk <junkerlucas@gmail.com>
2016-03-03 09:35:48 +0100

    LightTest : a test of 2D lighting on 3D objects

---
+++ Assets/Prototypes/LightTest/Scripts/Sphere2DController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere2DController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if(Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.left);
+            }
+            else if(Input.GetKey(KeyCode.RightArrow))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right);
+            }
+        }	
+	}
+}
