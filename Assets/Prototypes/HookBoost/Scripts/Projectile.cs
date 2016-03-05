7bdaccd0c91e2440a972d7ce802436c817564850
Nujerk <junkerlucas@gmail.com>
2016-03-05 11:03:29 +0100

    HookBoost : new prototype to test a hook that propulse the player

---
+++ Assets/Prototypes/HookBoost/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace HookBoost
+{
+    public class Projectile : MonoBehaviour
+    {
+        public GameObject projector;
+
+        public void OnCollisionEnter2D(Collision2D col)
+        {
+            foreach(var comp in gameObject.GetComponents<DistanceJoint2D>())
+            {
+                Destroy(comp);
+            }
+            projector.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+            var joint = projector.AddComponent<DistanceJoint2D>();
+            joint.connectedBody = col.rigidbody;
+            var x = (col.contacts[0].point.x - col.transform.position.x) / col.transform.localScale.x;
+            joint.connectedAnchor = new Vector2(x, 0);
+        }
+    }
+}
---
+++ Assets/Prototypes/HookBoost/Scripts/Sphere2DController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HookBoost
+{
+    public class Sphere2DController : MonoBehaviour
+    {
+        public GameObject projectile;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.anyKey)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.left);
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right);
+                }
+            }
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                var direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position;
+                var instance = GameObject.Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+                projectile.GetComponent<Projectile>().projector = gameObject;
+                // Use normalized vector to have a magnitude of 1 (this way even a little pressure on the joystick allow to shoot at full strength).
+                instance.GetComponent<Rigidbody2D>().AddForce(direction.normalized * 10, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
+
