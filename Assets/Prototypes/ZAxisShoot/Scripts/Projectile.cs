14988b19e4e48a357f9ee7723ad9004fc30952cf
Nujerk <junkerlucas@gmail.com>
2016-03-05 11:02:57 +0100

    ZAxisShoot : add namespace to scripts

--- Assets/Prototypes/ZAxisShoot/Scripts/Projectile.cs
+++ Assets/Prototypes/ZAxisShoot/Scripts/Projectile.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Projectile : MonoBehaviour {
+namespace ZAxisShoot
+{
+    public class Projectile : MonoBehaviour
+    {
 
-    private float birthDate;
-    public float lifeDuration = 2;
+        private float birthDate;
+        public float lifeDuration = 2;
 
-	// Use this for initialization
-	void Start () {
-        birthDate = Time.realtimeSinceStartup;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Time.realtimeSinceStartup - birthDate > lifeDuration)
-            Destroy(gameObject);
-	}
+        // Use this for initialization
+        void Start()
+        {
+            birthDate = Time.realtimeSinceStartup;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Time.realtimeSinceStartup - birthDate > lifeDuration)
+                Destroy(gameObject);
+        }
+    }
+
 }
