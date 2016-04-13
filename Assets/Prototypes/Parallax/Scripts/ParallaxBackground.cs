9d7f6159fc59dcc1b2ba4d5ff0f0145750c72ac6
Nujerk <junkerlucas@gmail.com>
2016-04-13 13:30:36 +0200

    Add Parallax prototype

---
+++ Assets/Prototypes/Parallax/Scripts/ParallaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Parallax
+{
+    public class ParallaxBackground : MonoBehaviour
+    {
+        [Range(1, 10)]
+        public float parallaxSpeed = 5;
+
+        private GameObject _player;
+
+        void Start()
+        {
+            _player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        void Update()
+        {
+            transform.position = new Vector3(_player.transform.position.x / (11 - parallaxSpeed), transform.position.y, transform.position.z);
+        }
+    }
+}
+
+
---
+++ Assets/Prototypes/Parallax/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Parallax
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * 10 * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * 10 * Time.deltaTime;
+            }
+        }
+    }
+}
+
+
