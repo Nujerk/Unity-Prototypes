cf8f6909a16a7d02dd88d86cc627ec98dc529cb8
Nujerk <junkerlucas@gmail.com>
2016-04-13 13:26:38 +0200

    Add ScreenShake prototype

---
+++ Assets/Prototypes/ScreenShake/Scripts/ScreenShakeOnContact.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ScreenShake
+{
+    public class ScreenShakeOnContact : MonoBehaviour
+    {
+
+        public GameObject target;
+        public Camera camera;
+
+        public void OnCollisionEnter2D(Collision2D coll)
+        {
+            if (coll.gameObject == target)
+                ScreenShake();
+        }
+
+        private void ScreenShake()
+        {
+            camera.GetComponent<Animator>().PlayInFixedTime("Shake");
+        }
+
+    }
+}
