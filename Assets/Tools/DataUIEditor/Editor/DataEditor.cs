463c68ee9dfbe48a545a412cd71bde393a510fc3
Nujerk <junkerlucas@gmail.com>
2016-06-14 13:49:20 +0200

    Work on a Data Editor

---
+++ Assets/Tools/DataUIEditor/Editor/DataEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace DataUIEditor
+{
+    public class DataEditor : EditorWindow
+    {
+        private Object folder;
+        private string file = "";
+
+        [MenuItem("Window/Data Editor")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(DataEditor));
+        }
+
+        void OnGUI()
+        {
+            if(folder != null)
+            {
+
+            }
+
+            GUILayout.Label("Settings", EditorStyles.boldLabel);
+            folder = EditorGUILayout.ObjectField("Data Folder :", folder, typeof(TextAsset), null);
+            
+        }
+    }
+
+}
