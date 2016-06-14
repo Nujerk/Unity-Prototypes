1e022c8ca7bf44995c5cbb5bf83d20d9d33c3e62
Nujerk <junkerlucas@gmail.com>
2016-06-14 13:48:36 +0200

    Work on physics : try to bounce enemy by jumping on it

---
+++ Assets/Prototypes/PhysicsTest/Scripts/BouncingEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PhysicsTest
+{
+    public class BouncingEnemy : MonoBehaviour
+    {
+        public bool isBouncing = false;
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (!isBouncing)
+                return;
+
+            var velocity = collision.relativeVelocity;
+            var normal = collision.contacts[0].normal;
+            var direction = new Vector2(velocity.x * Mathf.Sign(normal.x), velocity.y * Mathf.Sign(normal.y));
+
+            GetComponent<Rigidbody2D>().AddForce(direction, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Prototypes/PhysicsTest/Scripts/PlayerBumper.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PhysicsTest
+{
+    public class PlayerBumper : MonoBehaviour
+    {
+        public float moveSpeed = 5; // in pixels / s
+        public float jumpForce = 5;
+        public float pixelPerUnit = 10;
+
+        private bool _touchingGround;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.Space) && _touchingGround)
+            {
+                // Jump
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce / pixelPerUnit), ForceMode2D.Impulse);
+            }
+
+            var direction = Input.GetAxis("Horizontal");
+            if (Mathf.Abs(direction) > 0)
+            {
+                var forceX = moveSpeed / pixelPerUnit * Mathf.Sign(direction);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(forceX, GetComponent<Rigidbody2D>().velocity.y);
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                _touchingGround = true;
+
+            if(collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                var contact = collision.contacts[0].point;
+                var direction = contact - (Vector2)collision.gameObject.transform.position;
+                var normal = direction.normalized;
+                normal *= -20;
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity = normal;
+                collision.gameObject.GetComponent<BouncingEnemy>().isBouncing = true;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                _touchingGround = false;
+        }
+    }
+}
+
+
